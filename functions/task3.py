a0dd667c036ca64a42482045c93b245f340cf642
killu21 <mgrzelak218@gmail.com>
2024-12-06 17:08:07 +0100

    M1

--- functions/task3.py
+++ functions/task3.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Process of expending images, increasing brightness of the image if original image and pixel matches we replace part of image with mask
 def dilation(image, struct_elem):
@@ -31,3 +32,19 @@ def hmt(image, struct_elem1, struct_elem2):
     complement_image = cv2.bitwise_not(image)
     erosion_image2 = erosion(complement_image, struct_elem2)
     return cv2.bitwise_and(erosion_image1, erosion_image2)
+
+# (A ⊕ B) \ A - dilation subtracts A
+def operation_1(image, struct_elem):
+    dilated_image = dilation(image, struct_elem)
+    return cv2.subtract(dilated_image, image)
+
+# A \ (A ⊖ B) - A subtracts erosion
+def operation_2(image, struct_elem):
+    eroded_image = erosion(image, struct_elem)
+    return cv2.subtract(image, eroded_image)
+
+# (A ⊕ B) \ (A ⊖ B) - dilation subtracts erosion
+def operation_3(image, struct_elem):
+    dilated_image = dilation(image, struct_elem)
+    eroded_image = erosion(image, struct_elem)
+    return cv2.subtract(dilated_image, eroded_image)
