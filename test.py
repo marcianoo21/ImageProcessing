9bbf891758ae1bb093cd95596bfda9a9d5707523
killu21 <mgrzelak218@gmail.com>
2025-01-05 12:15:17 +0100

    s

--- test.py
+++ test.py
@@ -52,40 +52,14 @@ from PIL import Image
 image = Image.open("./images/lena.bmp").convert("L")  # Convert to grayscale
 image_array = np.array(image)
 
-# Apply 2D FFT
-# dft_result = np.fft.fft2(image_array)
-
-# # Shift zero frequency to the center
-# dft_result_shifted = np.fft.fftshift(dft_result)
-
-# # Compute magnitude spectrum
-# magnitude_spectrum = np.log(1 + np.abs(dft_result_shifted))
-
-# # Plot the original image and magnitude spectrum
-# plt.figure(figsize=(12, 6))
-
-# # Original Image
-# plt.subplot(1, 2, 1)
-# plt.title("Original Image")
-# plt.imshow(image_array, cmap="gray")
-# plt.axis("off")
-
-# # Magnitude Spectrum
-# plt.subplot(1, 2, 2)
-# plt.title("Magnitude Spectrum")
-# plt.imshow(magnitude_spectrum, cmap="gray")
-# plt.axis("off")
-
-# plt.show()
-
-
-idft_result = np.fft.ifft2(image_array)
+#Apply 2D FFT direct
+dft_result = np.fft.fft2(image_array)
 
 # Shift zero frequency to the center
-idft_result_shifted = np.fft.ifftshift(idft_result)
+dft_result_shifted = np.fft.fftshift(dft_result)
 
 # Compute magnitude spectrum
-magnitude_spectrum = np.log(1 + np.abs(idft_result_shifted))
+magnitude_spectrum = np.log(1 + np.abs(dft_result_shifted))
 
 # Plot the original image and magnitude spectrum
 plt.figure(figsize=(12, 6))
@@ -103,3 +77,30 @@ plt.imshow(magnitude_spectrum, cmap="gray")
 plt.axis("off")
 
 plt.show()
+
+#Apply 2D FFT inverse
+
+# idft_result = np.fft.ifft2(image_array)
+
+# # Shift zero frequency to the center
+# idft_result_shifted = np.fft.ifftshift(idft_result)
+
+# # Compute magnitude spectrum
+# magnitude_spectrum = np.log(1 + np.abs(idft_result_shifted))
+
+# # Plot the original image and magnitude spectrum
+# plt.figure(figsize=(12, 6))
+
+# # Original Image
+# plt.subplot(1, 2, 1)
+# plt.title("Original Image")
+# plt.imshow(image_array, cmap="gray")
+# plt.axis("off")
+
+# # Magnitude Spectrum
+# plt.subplot(1, 2, 2)
+# plt.title("Magnitude Spectrum")
+# plt.imshow(magnitude_spectrum, cmap="gray")
+# plt.axis("off")
+
+# plt.show()
